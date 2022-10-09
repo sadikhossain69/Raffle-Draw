9349c545168c026d9fe9cd5e446043b7c02f6bf0
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-09 22:03:37 +0600

    error handle done

--- src/server.js
+++ src/server.js
@@ -9,7 +9,23 @@ const app = express()
 app.use([morgan('dev'), cors(), express.json()])
 
 app.get('/health', (req, res) => {
-    res.status(400).json({message: 'success'})
+    res.status(400).json({ message: 'success' })
+})
+
+// Error Handle
+app.use((req, res, next) => {
+    const error = new Error("Resource Not Found")
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        return res.status(error.status).json({
+            message: error.message
+        })
+    }
+    res.status(500).json({ message: "Something Went Wrong" })
 })
 
 // Port
