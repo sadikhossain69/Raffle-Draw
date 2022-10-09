82b400bf86a1ad89ec53f58af3d139fefbe95a25
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-09 21:55:33 +0600

    colors is installed and server is listning at 5000 port

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const colors = require('colors')
 
 const app = express()
 
 // Middleware
 app.use([morgan('dev'), cors(), express.json()])
 
+// Port
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Raffle Draw app listening at http://localhost:${port}`.blue)
+})
